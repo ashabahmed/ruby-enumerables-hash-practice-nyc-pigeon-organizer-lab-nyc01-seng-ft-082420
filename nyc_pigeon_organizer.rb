5a4f27f7918ce0134b2ca5ed5d79a7b432c5dbc4
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-24 02:37:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  pigeon_array = {
-    "Theo" => {:color => ["purple", "grey"], :gender => ["male"]}, 
-    "Peter Jr" => {:color => ["purple", "grey"]}, 
-    "Lucky" => {:color => ["purple"]},
-    "Ms. K" => {:color => ["grey"]},
-    "Alex" => {:gender => ["male"]}
-  }
+  # pigeon_array = {
+  #   "Theo" => {:color => ["purple", "grey"], :gender => ["male"]}, 
+  #   "Peter Jr" => {:color => ["purple", "grey"]}, 
+  #   "Lucky" => {:color => ["purple"]},
+  #   "Ms. K" => {:color => ["grey"]},
+  #   "Alex" => {:gender => ["male"]}
+  # }
   
 
 end
