128798545d0b8f100cdf2d9056857e56ca49f44d
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-24 02:37:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,6 +7,24 @@ def nyc_pigeon_organizer(data)
   #   "Ms. K" => {:color => ["grey"]},
   #   "Alex" => {:gender => ["male"]}
   # }
+    pigeon_hash = {}
   
+  data.each do | key, value |
+    value.each do |next_key, names|
+      names.each do |name|
+      
+        if  !pigeon_hash[name]
+          pigeon_hash[name] = {}
+        end
+        
+        if !pigeon_hash[name][key]
+          pigeon_hash[name][key] = []
+        end
+
+        pigeon_hash[name][key].push(next_key.to_s)
+      end
+    end
+  end
+  pigeon_hash
 
 end
